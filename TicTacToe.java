d71b2c4e8a532ba935309961de2c26d6b66bb3d3
SKPradeepp <skpiitmember2008@gmail.com>
2026-05-09 11:15:20 +0530

    Coded UC8: Continuous turn-based game loop

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,4 +1,5 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class TicTacToe {
 
@@ -8,14 +9,55 @@ public class TicTacToe {
         {'-', '-', '-'}
     };
 
+    static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
 
-        computerMove('O');
+        boolean isUserTurn = true;
+        boolean gameOver = false;
+        int moves = 0;
+
+        while (!gameOver) {
+
+            printBoard();
+
+            if (isUserTurn) {
+
+                System.out.print("Enter slot (1-9): ");
+                int slot = scanner.nextInt();
+
+                int row = (slot - 1) / 3;
+                int col = (slot - 1) % 3;
+
+                if (board[row][col] == '-') {
+                    board[row][col] = 'X';
+                    moves++;
+                    isUserTurn = false;
+                } else {
+                    System.out.println("Cell already occupied!");
+                }
+
+            } else {
+
+                computerMove();
+                moves++;
+                isUserTurn = true;
+            }
+
+            // Stop game after all 9 moves (draw condition placeholder)
+            if (moves == 9) {
+                gameOver = true;
+                System.out.println("Game Draw!");
+            }
+        }
 
         printBoard();
     }
 
-    static void computerMove(char symbol) {
+    /**
+     * Computer makes random move
+     */
+    static void computerMove() {
 
         Random random = new Random();
 
@@ -27,13 +69,19 @@ public class TicTacToe {
             int col = (slot - 1) % 3;
 
             if (board[row][col] == '-') {
-                board[row][col] = symbol;
+
+                board[row][col] = 'O';
+
                 System.out.println("Computer selected slot: " + slot);
+
                 break;
             }
         }
     }
 
+    /**
+     * Prints the board
+     */
     static void printBoard() {
 
         System.out.println("-------------------");
